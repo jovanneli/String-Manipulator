453f49b73d06d5367bcb3c4a59393ec7915bd109
Jovi <42977182+jovanneli@users.noreply.github.com>
2018-10-25 13:36:27 -0400

    Add files via upload

--- src/StringManipulatorTest.java
+++ src/StringManipulatorTest.java
@@ -7,10 +7,13 @@ public class StringManipulatorTest
 	{	
 		StringManipulator stringManipulator = new StringManipulator();
 		Scanner in = new Scanner(System.in);
-		System.out.println("Indicate which method you would like to run by "
-							+ "entering 1 for the noVowels method and 2 for the reverse method: "); 
-							int method = in.nextInt();
-							in.nextLine();
+		boolean manipulator = true;
+		while (manipulator)
+		{
+			System.out.println("Indicate which method you would like to run by "
+								+ "entering 1 for the noVowels method and 2 for the reverse method: "); 
+								int method = in.nextInt(); in.nextLine();
+							
 		boolean play = true;
 		while (play)
 		{
@@ -32,41 +35,27 @@ public class StringManipulatorTest
 			}
 		}
 
-		System.out.println("Would you like to try again? Press 3 for yes and 4  for no: "); 
-							int yesOrNo = in.nextInt(); in.nextLine();
+		System.out.println("Would you like to try again? (enter yes or no) "); 
+							String yesOrNo = in.nextLine();
 		
 		boolean tryAgain = true;
 		while (tryAgain)
 		{
-			if (yesOrNo == 3)
+			if (yesOrNo.equals("yes"))
 			{
-				System.out.println("Indicate which method you would like to run by "
-					+ "entering 1 for the noVowels method and 2 for the reverse method: "); 
-					method = in.nextInt();
-					in.nextLine();
-				if (method == 1)
-				{
-					System.out.println("Please enter a word or phrase: "); String randomString = in.nextLine();
-					System.out.println("Manipulated string: " + stringManipulator.noVowels(randomString));
-				}
-				else if (method == 2)
-				{
-					System.out.println("Please enter a word or phrase: ");; String randomString2 = in.nextLine(); 
-					System.out.println("Manipulated string: " + stringManipulator.reverse(randomString2));
-				}
-				
-				System.out.println("Would you like to try again? Press 3 for yes and 4  for no: "); 
-									yesOrNo = in.nextInt(); in.nextLine();
+				tryAgain = false;
 			}
-			else if (yesOrNo == 4)
+			else if (yesOrNo.equals("no"))
 			{
-					tryAgain = false;
-					System.out.println("Thanks for playing!");
+				tryAgain = false;
+				manipulator = false;
+				System.out.println("Thanks for playing!");
 			}
 			else
 			{
-				System.out.print("Not an acceptable number. Try again: "); yesOrNo = in.nextInt(); in.nextLine();
+				System.out.print("Sorry, that is an invalid input. Try again: "); yesOrNo = in.nextLine();
 			}
+		}
 		}
 	}
 }
